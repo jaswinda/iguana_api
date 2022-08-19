74f50bd2fe9c9285da7b51eacb118886afa5b85d
jaswinda <singhjassi2624@gmail.com>
2022-08-20 01:49:52 +0545

    docker ci cd test 6

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -17,8 +20,22 @@ func main() {
 		c.String(http.StatusOK, "Hello from jaswindar %s", firstname)
 	})
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello to the ")
+		//ready .env file
+		Host := goDotEnvVariable("DB_USER")
+
+		c.String(http.StatusOK, "Hello from jaswindar %s", Host)
 	})
 
-	router.Run(":8000")
+	router.Run(":9000")
+}
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
